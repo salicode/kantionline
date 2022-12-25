57d61876446f0f2ae14488325375cffc37932ba7
Salisu Aminu Abdurrahman <salisuaminu615@gmail.com>
2022-12-25 17:33:29 +0100

    product apis created

--- store/serializer.py
+++ store/serializer.py
@@ -3,15 +3,19 @@ from rest_framework import serializers
 
 from store.models import Product,Collection
 
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields =['id', 'title', 'slug', 'inventory', 'unit_price']
 
-
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'description',  'unit_price', 'price_with_tax', 'collection', 'inventory']
+    
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
     collection = serializers.StringRelatedField()
-    # collection = serializers.PrimaryKeyRelatedField(query_set=Collection.objects.all())
+    
     
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from http import server
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse 
 from rest_framework.decorators import api_view
@@ -8,16 +9,35 @@ from .serializer import ProductSerializer
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
+   if request.method == 'GET':
     query_set = Product.objects.select_related('collection').all()
     serializer = ProductSerializer(query_set, many=True)
     return Response(serializer.data)
-
-
-@api_view()
-def product_detail(request, id):
-        product = get_object_or_404(Product, pk=id)
-        serializer = ProductSerializer(product)
-        return Response(serializer.data)
+   elif  request.method == 'POST':
+      serializer = ProductSerializer(data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_201_CREATED)
      
+     
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, id):
+ product = get_object_or_404(Product, pk=id)
+ if request.method == 'GET':
+      serializer = ProductSerializer(product)
+      return Response(serializer.data)
+ elif request.method == 'PUT':
+      serializer = ProductSerializer(product, data=request.data)
+      serializer.is_valid(raise_exception=True)
+      serializer.save()
+      return Response(serializer.data, status=status.HTTP_200_OK)
+ elif request.method == 'DELETE':
+   if product.orderitems.count() > 0:
+       return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+   product.delete()
+   return Response({'error': 'Product can not be deleted because it is associated with an order item'}, status=status.HTTP_204_NO_CONTENT)
+      
+      
